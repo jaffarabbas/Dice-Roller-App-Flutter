8fa635b245fe6697c69f1f5d822aa09c1b2a2f90
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-14 21:07:00 +0500

    Commited File : assets.dart at 6/14/2023 9:07:00 PM

--- flutter_dise_roller_app/lib/constants/assets.dart
+++ flutter_dise_roller_app/lib/constants/assets.dart
@@ -1,8 +1,8 @@
 class Assets {
-  static const String d1 = "images/d1.png";
-  static const String d2 = "images/d2.png";
-  static const String d3 = "images/d3.png";
-  static const String d4 = "images/d4.png";
-  static const String d5 = "images/d5.png";
-  static const String d6 = "images/d6.png";
+  static const String d1 = "assets/images/d1.png";
+  static const String d2 = "assets/images/d2.png";
+  static const String d3 = "assets/images/d3.png";
+  static const String d4 = "assets/images/d4.png";
+  static const String d5 = "assets/images/d5.png";
+  static const String d6 = "assets/images/d6.png";
 }
